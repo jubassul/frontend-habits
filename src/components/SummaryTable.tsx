69ac55dc57661c14a736e0c5ef9a8e6a92d09fd1
jubassul <jubassul@gmail.com>
2023-02-15 16:50:27 +0000

    create habit tracker interface

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,59 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import { generateRangeBetweenDates } from "../utils/generate-range-between-dates";
+import { HabitDay } from "./HabitDay";
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateRangeBetweenDates();
+const minimumSumaryDatesSize = 18 * 7; //18 weeks
+const amountOfDaysToFill = minimumSumaryDatesSize - summaryDates.length; //126-16 (110 só para aparecer)
+
+type Summary = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>
+
+
+export function SummaryTable() {
+  
+  const [summary, setSummary] = useState <Summary>([])
+  useEffect(()=> {
+   api.get('summary').then(response=>{
+    setSummary(response.data)
+   })
+
+  }, [])
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 font-bold text-xl h-10 w-10 flex items-center justify-cente"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+         const dayInSummary= summary.find(day=>{
+          return dayjs(date).isSame(day.date, 'day')
+         })
+         return <HabitDay date={date}  completed={dayInSummary?.completed} amount={dayInSummary?.amount} />;
+        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map(() => {
+            return (
+              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
